2755cda067014da4160d331006ec5edbe9264f68
Lisekho <lisekhosmith561@gmail.com>
2026-05-19 03:34:40 -0700

    Add files via upload

---
+++ ex1 basics.js
@@ -0,0 +1,15 @@
+//first name:
+const firstName= "Lisekho";
+//Last Name:
+const lastName= "Smith";
+//age:
+let age= 24;
+//Full Name:
+const fullName=  firstName + ' ' + lastName;
+//Greeting print:
+console.log(("Hello, my name is " + fullName + " and I am " + age + " years old."));
+//age variable update:
+age= 25;
+
+
+
